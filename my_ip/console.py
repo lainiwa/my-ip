d666f2a1578bb97ce4bfa136bc2655d1dfe15f42
lainiwa <kirrik96@gmail.com>
2019-09-27 11:26:53 +0300

    Utilize click library for CLI
    However mypy is not fully satisfied yet

--- my_ip/console.py
+++ my_ip/console.py
@@ -3,15 +3,20 @@
 The module specifies the script's CLI.
 """
 
+import os
 import asyncio
-from typing import Optional
+import pkgutil
+from typing import Any, Union
+
+import toml
 
 import click
+from my_ip import __version__
 from my_ip.core import get_ip
-from my_ip.settings import get_settings, default_config
+from my_ip.settings import Settings
 
 
-def print_ip(path: Optional[str] = None) -> None:
+def print_ip(settings: Settings) -> None:
     """Get current IP address and print it.
 
     Exit with non-zero code on failure.
@@ -20,7 +25,6 @@ def print_ip(path: Optional[str] = None) -> None:
         path: Location of the script's configuration file.
 
     """
-    settings = get_settings(path)
     services = settings.service.values()
     loop = asyncio.get_event_loop()
     coro = get_ip(services)
@@ -30,19 +34,65 @@ def print_ip(path: Optional[str] = None) -> None:
     print(ip)
 
 
+def print_version(
+    ctx: click.core.Context,
+    param: Union[click.core.Option, click.core.Parameter],
+    value: Any,
+) -> Any:
+    """Print version click callback."""
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(__package__ + " version: " + __version__)
+    ctx.exit()
+
+
 @click.command()
 @click.option(
+    "--version",
+    is_flag=True,
+    callback=print_version,
+    expose_value=False,
+    is_eager=True,
+    help="print package version and exit",
+)
+@click.option(
     "--config",
-    show_default=True,
-    type=click.Path(exists=True),
-    default=default_config(),
+    type=click.Path(
+        exists=True,
+        dir_okay=False,
+        readable=True,
+        resolve_path=True,
+        allow_dash=True,
+    ),
+    default=None,
     help="path to config file",
 )
 def cli(config: str) -> None:
-    """Click library command.
+    """Find out your internet IP address."""
+    std_config_dir = click.get_app_dir(__package__)
+    std_config_path = os.path.join(std_config_dir, "config.toml")
 
-    Parameters:
-        config: location of the configuration file.
+    if not os.path.isfile(std_config_path):
+        click.echo("First run.")
+        click.echo(f"Installing config to `{std_config_path}`... ", nl=False)
 
-    """
-    print_ip(config)
+        os.makedirs(std_config_dir, exist_ok=True)
+
+        with open(std_config_path, "w") as file:
+            file.write(
+                pkgutil.get_data(__package__, "data/config.toml").decode(
+                    "utf-8"
+                )
+            )
+
+        click.echo("Done!")
+
+    if config is None:
+        config = std_config_path
+
+    with click.open_file(config, "r") as fil:
+        print_ip(Settings(**toml.loads(fil.read())))
+
+
+if __name__ == "__main__":
+    cli()
--- my_ip/settings.py
+++ my_ip/settings.py
@@ -2,15 +2,9 @@
 
 This module defines appropriate pydantic models for config file. This lets the
 script to validate the settings while parsing them.
-
-The module also defines a function specifying the right place to search for
-the config, respecting XDG Base Directory Specification.
 """
 
-import os
 from typing import Dict, Optional
-
-import toml
 
 from pydantic import UrlStr, BaseModel
 
@@ -37,30 +31,3 @@ class Settings(BaseModel):
     """
 
     service: Dict[str, Service]
-
-
-def default_config() -> str:
-    """Config default path.
-
-    Returns:
-        Expected path of the configuration file.
-
-    """
-    xdg_config_home = os.getenv("$XDG_CONFIG_HOME", "~/.config")
-    unexpanded_tilde_config = os.path.join(xdg_config_home, "my_ip.toml")
-    return os.path.expanduser(unexpanded_tilde_config)
-
-
-def get_settings(path: Optional[str]) -> Settings:
-    """Parse and validate configuration file.
-
-    Parameters:
-        path: configuration file location.
-
-    Returns:
-        Model of the configuration file.
-
-    """
-    if path is None:
-        path = default_config()
-    return Settings(**toml.load(path))
