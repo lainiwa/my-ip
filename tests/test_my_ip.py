329626740556ce4f141f2c4ae68d124e6f7c8496
lainiwa <kirrik96@gmail.com>
2019-11-07 02:04:48 +0300

    Add cli version test

--- tests/test_my_ip.py
+++ tests/test_my_ip.py
@@ -1,8 +1,19 @@
 """Tests for my_ip package."""
 
+from click.testing import CliRunner
+
 from my_ip import __version__
+from my_ip.console import cli
 
 
 def test_version() -> None:
     """Test for version."""
     assert __version__ == "0.2.0"
+
+
+def test_cli_version():
+    """Test `mip --version` output."""
+    runner = CliRunner()
+    result = runner.invoke(cli, ["--version"])
+    assert result.exit_code == 0
+    assert result.output == "my_ip version: 0.2.0\n"
